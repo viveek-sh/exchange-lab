b5bce313b1c03ff6b3b017245a7fb96edcd0e023
viveek-sh <get.viveksahu@gmail.com>
2026-02-24 21:33:45 +0530

    add response,message and order types

--- apps/api/src/types/orderSchema.ts
+++ apps/api/src/types/orderSchema.ts
@@ -1,21 +1,27 @@
 import { z } from "zod";
 
-export const orderInputSchema = z.object({
+export const createOrderSchema = z.object({
   baseAsset: z
     .string()
     .min(1)
     .regex(/^[A-Z]+$/),
-
   quoteAsset: z
     .string()
     .min(1)
     .regex(/^[A-Z]+$/),
-
-  side: z.enum(["buy", "sell"]), //bid = buy, ask = sell
+  side: z.enum(["buy", "sell"]),
   orderType: z.enum(["limit", "market"]),
   price: z.number().positive(),
   quantity: z.number().positive(),
   executionType: z.enum(["ioc"]).optional(),
 });
 
-export type orderDataType = z.infer<typeof orderInputSchema>;
+export const cancelOrderSchema = z.object({
+  orderId: z.string().min(1),
+  market: z.string().min(1),
+});
+
+export const getOpenOrdersSchema = z.object({
+  userId: z.string().min(1),
+  market: z.string().min(1),
+});
