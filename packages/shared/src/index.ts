83600c2a07e271bac001d21132107e7f896dd120
viveek-sh <get.viveksahu@gmail.com>
2026-07-01 14:19:47 +0530

    :feat(shared) : add types for web UI

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,4 +2,5 @@ export * from "./engineEvents.js";
 export * from "./engineProtocol.js";
 export * from "./wsMessages.js";
 export * from "./dbMessages.js";
+export * from "./webUI.js";
 export const BASE_CURRENCY = "INR";
